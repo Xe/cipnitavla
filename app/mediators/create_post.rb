c3d4eabe4716e7cd3221a14c0cb74445721d6f0d
Christine Dodrill <cdodrill@salesforce.com>
2018-11-20 09:50:06 -0800

    this isn't haskell

--- app/mediators/create_post.rb
+++ app/mediators/create_post.rb
@@ -1,5 +1,5 @@
-module Mediators where
-  class CreatePost where
+module Mediators
+  class CreatePost
     def run(args)
       Post.create!({body: args["body"], author: args["user_id"]}) # or potentially just Post.create!(args)
     end
